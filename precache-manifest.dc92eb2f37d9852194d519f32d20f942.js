b3db3da7aaa1c7780d939fd1bdf061d09410c68b
Nightlue2 <basicperfect@outlook.com>
2020-10-18 17:48:30 +0800

    deploy

---
+++ precache-manifest.dc92eb2f37d9852194d519f32d20f942.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa2061b31110c4b5c78",
+    "url": "/XCMoney/css/app.07712527.css"
+  },
+  {
+    "revision": "bcdd2104dbcc2b29146726bf92f14a5d",
+    "url": "/XCMoney/index.html"
+  },
+  {
+    "revision": "dfa2061b31110c4b5c78",
+    "url": "/XCMoney/js/app.e2fa8e72.js"
+  },
+  {
+    "revision": "3b7240c3b1278148eb58",
+    "url": "/XCMoney/js/chunk-vendors.95618d70.js"
+  },
+  {
+    "revision": "0b12f397bb105b2cd373284f74877e15",
+    "url": "/XCMoney/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/XCMoney/robots.txt"
+  }
+]);
